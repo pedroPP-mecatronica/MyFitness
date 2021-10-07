199251a399da16cb547746a52b0a57b9250d6b1e
pedroPP-mecatronica <PedroGaldino>
2021-10-07 12:43:08 -0300

    CALCULOS DE IMC

--- app/src/main/java/com/example/myfitness/ImcActivity.kt
+++ app/src/main/java/com/example/myfitness/ImcActivity.kt
@@ -1,18 +1,50 @@
 package com.example.myfitness
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_imc.*
+import java.lang.NumberFormatException
 
 
-
-class ImcActivity : AppCompatActivity() {
+class ImcActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+        btn_calcular.setOnClickListener(this)
     }
 
+    fun calculate() {
+        if (validationOk())
+        {
+            try {
+                val imc: Float =
+                    (edit_peso.text.toString().toFloat()) / (edit_altura.text.toString().toFloat() * edit_altura.text.toString().toFloat())
+                if (imc < 15) Toast.makeText(this, "Muito está muito Magro(a)", Toast.LENGTH_SHORT).show()
+                if (imc > 15 && imc < 25) Toast.makeText(this, "Normal", Toast.LENGTH_SHORT).show()
+                if (imc > 25 && imc < 28) Toast.makeText(this, "Sobre Peso", Toast.LENGTH_SHORT).show()
+                if (imc > 28) Toast.makeText(this, "Sobre Peso", Toast.LENGTH_SHORT).show()
+                }
+            catch (nfe:NumberFormatException)
+            {
+                Toast.makeText(this,"Valor inválido",Toast.LENGTH_SHORT).show()
+            }
+        }
+        else
+        {
+            Toast.makeText(this,"Preencha Todos os Campos",Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    private fun validationOk(): Boolean {
+        return (!edit_altura.text.isNullOrBlank() && !edit_peso.text.isNullOrBlank())
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.btn_calcular) {
+            calculate()
+        }
+    }
 }
--- app/src/main/java/com/example/myfitness/MainActivity.kt
+++ app/src/main/java/com/example/myfitness/MainActivity.kt
@@ -7,16 +7,19 @@ import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnImc:Button=findViewById(R.id.btn_calcula_imc)
         btnImc.setOnClickListener(this)
     }
+
     override fun onClick(view: View) {
         val id= view.id
         if(id==R.id.btn_calcula_imc){
             startActivity(Intent(this,ImcActivity::class.java))
         }
     }
+
 }
