008480099ae9c3744550d159eabeb255f0ef1da5
pedroPP-mecatronica <PedroGaldino>
2021-10-08 17:35:46 -0300

    CALCULOS DE IMC

--- app/src/main/java/com/example/myfitness/ImcActivity.kt
+++ app/src/main/java/com/example/myfitness/ImcActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myfitness
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_imc.*
 import java.lang.NumberFormatException
@@ -22,16 +24,7 @@ class ImcActivity : AppCompatActivity(), View.OnClickListener {
                 val imc: Float =
                     (edit_peso.text.toString().toFloat()) / (edit_altura.text.toString()
                         .toFloat() * edit_altura.text.toString().toFloat())
-                if (imc < 15) {
-                    val dialog: AlertDialog = AlertDialog.Builder(this)
-                        .setTitle(getString(R.string.imc_result, imc))
-                        .create()
-                    dialog.show()
-                }
-                if (imc > 15 && imc < 25) Toast.makeText(this, "Normal", Toast.LENGTH_SHORT).show()
-                if (imc > 25 && imc < 28) Toast.makeText(this, "Sobre Peso", Toast.LENGTH_SHORT)
-                    .show()
-                if (imc > 28) Toast.makeText(this, "Sobre Peso", Toast.LENGTH_SHORT).show()
+                createAlertDialog(escolherMensagem(imc), imc)
             } catch (nfe: NumberFormatException) {
                 Toast.makeText(this, "Valor inválido", Toast.LENGTH_SHORT).show()
             }
@@ -39,6 +32,35 @@ class ImcActivity : AppCompatActivity(), View.OnClickListener {
             Toast.makeText(this, "Preencha Todos os Campos", Toast.LENGTH_SHORT).show()
         }
     }
+
+    private fun createAlertDialog(escolherMensagem: Int, imc: Float) {
+        val dialog: AlertDialog =
+            AlertDialog
+                .Builder(this)
+                .setTitle(getString(R.string.imc_response, imc))
+                .setMessage(escolherMensagem)
+                .setPositiveButton(android.R.string.ok,
+                    DialogInterface.OnClickListener { dialog, id -> })
+                .setNegativeButton(android.R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id -> })
+                .create()
+        dialog.show()
+    }
+
+    @StringRes
+    private fun escolherMensagem(imc: Float): Int {
+        return if (imc < 15)
+            R.string.imc_severely_low_weight
+        else if (imc > 15 && imc < 23)
+            R.string.imc_low_weight
+        else if (imc > 25 && imc < 28)
+            R.string.normal
+        else if (imc > 28 && imc < 30)
+            R.string.imc_high_weight
+        else
+            R.string.imc_extreme_weight
+    }
+
 
     private fun validationOk(): Boolean {
         return (!edit_altura.text.isNullOrBlank() && !edit_peso.text.isNullOrBlank())
